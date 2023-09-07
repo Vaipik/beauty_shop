0464cabdd2f5e49eac46d173cff9960bdeb4c506
Vaipik <n.khylko@gmail.com>
2023-09-07 16:07:12 +0300

    order item

---
+++ src/core/order/models/items.py
@@ -0,0 +1,43 @@
+import decimal
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from core.base.models import Base
+from core.product.constants import PRODUCT_PRICE_MAX_DIGITS
+
+
+class OrderItem(Base):
+    """Order item."""
+
+    price = models.DecimalField(
+        verbose_name=_("Price"),
+        max_digits=PRODUCT_PRICE_MAX_DIGITS,
+        decimal_places=2,
+    )
+    quantity = models.PositiveSmallIntegerField(
+        verbose_name=_("Item quantity"),
+        default=1,
+    )
+    order = models.ForeignKey(
+        to="Order", on_delete=models.SET_NULL, related_name="items"
+    )
+    product = models.ForeignKey(
+        to="core.Product",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="order_items",
+    )
+
+    class Meta:
+        db_table = "order_items"
+        verbose_name = "Order item"
+        verbose_name_plural = "Order items"
+
+    def __str__(self) -> str:
+        return f"{self.product} [{self.price=}, {self.quantity=}]"
+
+    @property
+    def items_cost(self) -> decimal.Decimal:
+        """Total price for item according to its quantity."""
+        return self.quantity * self.price
