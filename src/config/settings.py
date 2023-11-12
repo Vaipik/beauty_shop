203d4c39978cb990f1bd2a557b7d5b61e17e8ac1
Vaipik <n.khylko@gmail.com>
2023-11-12 10:13:29 +0200

    delete admin urls because is_staff field was deleted

--- src/config/settings.py
+++ src/config/settings.py
@@ -115,7 +115,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # AUTH
 AUTH_USER_MODEL = "user_auth.User"
-AUTHENTICATION_BACKENDS = ("user_auth.backends.AuthBackend",)
+AUTHENTICATION_BACKENDS = ("core.user_auth.backends.AuthBackend",)
 
 
 STORAGES = {
