b75701207a17e6247cb2c17db9af0e9b93cd693a
Vaipik <n.khylko@gmail.com>
2023-11-18 12:12:48 +0200

    product factories init

---
+++ src/core/product/factories/manufacturer.py
@@ -0,0 +1,13 @@
+import factory.fuzzy
+
+from core.product.models import ProductManufacturer
+
+
+class ProductManufacturerFactory(factory.django.DjangoModelFactory):
+    """Factory for manufacturer."""
+
+    name = factory.Sequence(lambda x: f"Manufacturer_{x}")
+    description = factory.LazyAttribute(lambda x: f"{x.name} description")
+
+    class Meta:
+        model = ProductManufacturer
---
+++ src/core/product/factories/product.py
@@ -0,0 +1,32 @@
+import factory.fuzzy
+
+from core.product import constants
+from core.product.models import Product
+
+from .manufacturer import ProductManufacturerFactory
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    """Product factory."""
+
+    name = factory.Sequence(lambda x: f"Product_{x}")
+    description = factory.fuzzy.FuzzyText()
+    rating = factory.fuzzy.FuzzyDecimal(
+        low=0,
+        high=5,
+    )
+    price = factory.fuzzy.FuzzyDecimal(
+        low=0,
+        high=int("9" * (constants.PRODUCT_PRICE_MAX_DIGITS - 2)),  # 9999999 without .XX
+    )
+    sku = factory.fuzzy.FuzzyInteger(low=0, high=2147483647)  # PositiveIntegerField
+    status = factory.fuzzy.FuzzyChoice(Product.ProductStatusChoices.values)
+    sibling_name = factory.fuzzy.FuzzyText(length=6)  # noqa
+    main_card = False  # To override when instantiating just pass main_card=True
+    is_luxury = True  # same as above
+    manufacturer = factory.SubFactory(ProductManufacturerFactory)
+    # categories = factory.SubFactory(...)
+    # options = factory.SubFactory(...)
+
+    class Meta:
+        model = Product
