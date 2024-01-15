14abf4da6f3249cc9f5a8b71115ce77253892b23
Vaipik <n.khylko@gmail.com>
2024-01-15 22:04:49 +0200

    signals were moved into core app

--- src/api/v1/cart/apps.py
+++ src/api/v1/cart/apps.py
@@ -5,6 +5,3 @@ class CartConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "api.v1.cart"
     label = "api_v1_cart"
-
-    def ready(self):
-        import api.v1.cart.signals
